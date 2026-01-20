4c953c607736ebb321bbe24072a49aec8652b012
Jacson Silva <Jacson.Silva@gwcloud.company>
2026-01-20 13:50:16 -0300

    fix: resolve problem cors

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { SportVirtualModule } from './modules/sport-virtual/sport-virtual.module
 
 (async () => {
   const app = await NestFactory.create(SportVirtualModule);
+
+  app.enableCors({
+    origin: '*', // ⚠️ aberto para qualquer domínio
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+  });
+
   await app.listen(process.env.PORT ?? 5000);
 })();
